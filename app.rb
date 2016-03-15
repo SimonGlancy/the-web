35dae11eada93a1677faef4a639815300fd895cf
rhiannonruth <rhiannonruth@hotmail.co.uk>
2016-03-15 14:15:30 +0000

    get/post methods for named-cat

--- app.rb
+++ app.rb
@@ -17,7 +17,11 @@ get '/random-cat' do
   erb(:index)
 end
 
-get '/named-cat' do
+get '/named-cat-form' do
+  erb(:cat_form)
+end
+
+post '/named-cat' do
   p params
   @name = params[:name]
   erb(:index)
