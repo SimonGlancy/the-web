fd234b6b51ac1a551c58d658efdef4c95c19157b
Simon Glancy <simonglancy@Simons-MacBook-Pro-3.local>
2016-03-15 10:02:23 +0000

    start of day-two

--- app.rb
+++ app.rb
@@ -11,3 +11,7 @@ end
 get '/name/time' do
   "its #{Time.now.strftime("%H:%M")} Patrick"
 end
+
+get '/cat' do
+  http://f.cl.ly/items/0k0v3e2X3l2f3i1n1Y19/Screen%20Shot%202013-09-10%20at%2011.32.00.png
+end
