1747d2cdd360b16dd197f0ec3a1df0b22b1cba86
Simon Glancy <simonglancy@Simons-MacBook-Pro-3.local>
2016-03-15 10:41:30 +0000

    we gotz a kat

--- app.rb
+++ app.rb
@@ -13,5 +13,7 @@ get '/name/time' do
 end
 
 get '/cat' do
-  http://f.cl.ly/items/0k0v3e2X3l2f3i1n1Y19/Screen%20Shot%202013-09-10%20at%2011.32.00.png
+  '<div style="border:5px Dashed Red; width: 600px; height: 300px;">
+  <img src=http://bit.ly/1eze8aE >
+  </div>'
 end
